fa289c39cfd14d9d91b3b7c9476658dd69c7a3f3
Joey-Cerulli <cerullij@union.edu>
2026-01-13 16:45:48 -0500

    removed while loop, tested printing function

--- project1/main/main.c
+++ project1/main/main.c
@@ -1,10 +1,31 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
 
+
 #define dseat GPIO_NUM_10  //Driver seat button pin
 #define dbelt GPIO_NUM_11  //Driver seatbelt button pin
 #define pseat GPIO_NUM_12  //Passenger seat button pin
 #define pbelt GPIO_NUM_13  //Passenger seat button pin
+
+
+void print_status() {                               //Define a function for printing reason for car not starting
+    if (gpio_get_level(dseat)){                     //Check if driver is seated
+        printf("Driver Not Seated \n");             //Print if driver not seated
+    }
+
+    if (gpio_get_level(dbelt)){                     //Check if driver is buckled
+        printf("Driver Not Buckled \n");            //Print if driver not buckled
+    }
+
+    if (gpio_get_level(pseat)){                     //Check if passenger is seated
+        printf("Passenger Not Seated \n");          //Print if passenger not seated
+    }
+
+    if (gpio_get_level(dbelt)){                     //Check if passenger is buckled
+        printf("Passenger Not Buckled \n");         //Print if passenger not buckled
+    }
+}
+
 
 void app_main(void) {
 
@@ -32,27 +53,8 @@ void app_main(void) {
     gpio_pullup_en(pbelt);
     gpio_pulldown_en(pbelt);
     gpio_intr_disable(pbelt);
+    
+    
+    print_status();
 
-    while(true) {                                   //Run continuously
-
-        vTaskDelay(20 / portTICK_PERIOD_MS);        // Loop Delay
-    }
-}
-
-void print_status() {                               //Define a function for printing reason for car not starting
-    if (gpio_get_level(dseat)){                     //Check if driver is seated
-        printf("Driver Not Seated \n");             //Print if driver not seated
-    }
-
-    if (gpio_get_level(dbelt)){                     //Check if driver is buckled
-        printf("Driver Not Buckled \n");            //Print if driver not buckled
-    }
-
-    if (gpio_get_level(pseat)){                     //Check if passenger is seated
-        printf("Passenger Not Seated \n");          //Print if passenger not seated
-    }
-
-    if (gpio_get_level(dbelt)){                     //Check if passenger is buckled
-        printf("Passenger Not Buckled \n");         //Print if passenger not buckled
-    }
 }
